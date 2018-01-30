381b4b1d08be2cf064fc1a561ad467f7ace3d78f
Ethan Anderes <ethananderes@Ethans-MacBook-Pro-2.local>
2018-01-30 06:39:58 -0800

    saving state

--- src/util.jl
+++ src/util.jl
@@ -1,4 +1,6 @@
 
+
+
 @generated data(x::Field) = :(tuple($((:(x.$f) for f=fieldnames(x))...)))
 
 @inline squash(x::T) where T = ifelse(isfinite(x), x, T(0))
@@ -6,3 +8,5 @@
 function white_noise(g::HarmonicTransform{P,T}) where {T,P<:Flat{θ,n}} where {θ,n}
     randn(T,n,n) ./ sqrt(g.Ωx)
 end
+
+
