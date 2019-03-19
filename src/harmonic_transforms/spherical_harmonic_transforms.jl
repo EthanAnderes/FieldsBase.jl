b7b7375e39b7157802dea0c58fa944ffbb18f4fc
Ethan Anderes <anderes@ucdavis.edu>
2019-03-19 09:22:31 +0100

    Update spherical_harmonic_transforms.jl

--- src/harmonic_transforms/spherical_harmonic_transforms.jl
+++ src/harmonic_transforms/spherical_harmonic_transforms.jl
@@ -4,11 +4,11 @@
 #  Healpix transform
 #############################################
 # real transform
- struct rℍ{P<:Healpix,T<:Real} <: HarmonicTransform{P,T}
-    Ωx::T
-    lmax::T
-    l::Matrix{T}
-    m::Matrix{T}
-    φ::Matrix{T}  # azmuth
-    Θ::Matrix{T}  # polar
-end
+# struct rℍ{P<:Healpix,T<:Real} <: HarmonicTransform{P,T}
+#    Ωx::T
+#    lmax::T
+#    l::Matrix{T}
+#    m::Matrix{T}
+#    φ::Matrix{T}  # azmuth
+#    Θ::Matrix{T}  # polar
+#end
